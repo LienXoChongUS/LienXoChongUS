8121f6774169684c3ed11f9cc1f0cfeae2b17bfb
qqhieubb <122640990+qqhieubb@users.noreply.github.com>
2023-12-07 16:34:16 +0700

    Category and Connection String DB

--- LienXoChongUS/Models/Category.cs
+++ LienXoChongUS/Models/Category.cs
@@ -1,6 +1,17 @@
-﻿namespace LienXoChongUS.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LienXoChongUS.Models
 {
 	public class Category
 	{
+
+		[Key] //đây là chú thích dữ liệu, nó dùng để xác định khóa chính,
+			  //nếu tên là ID thì chương trình tự xác định, còn không thì phải dùng chú thích dữ liệu
+		public int Id { get; set; }
+		[Required]
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public int DisplayOrder { get; set; }
+
 	}
 }
