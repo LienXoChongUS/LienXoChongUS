01198d7afa7ff0682b5842e608ce3238e16dc658
Tyends41 <141699209+Tyends41@users.noreply.github.com>
2023-12-09 02:30:19 +0700

    Tạo CategoryController, một số category mẫu, và CategoryIndex

--- LienXoChongUS/Data/AppDbContext.cs
+++ LienXoChongUS/Data/AppDbContext.cs
@@ -11,5 +11,14 @@ namespace LienXoChongUS.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", Description = "Funny", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", Description = "Funny", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Romance", Description = "Romantic", DisplayOrder = 3 }
+                ); 
+        }
     }
 }
