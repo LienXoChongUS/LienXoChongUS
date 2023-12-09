649b82c2fc29aa5b6efca8c6084a2850ae0cf014
qqhieubb <122640990+qqhieubb@users.noreply.github.com>
2023-12-09 22:33:42 +0700

    HIển thị list Category

--- LienXoChongUS/Controllers/CategoryController.cs
+++ LienXoChongUS/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LienXoChongUS.Data;
+using LienXoChongUS.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LienXoChongUS.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly AppDbContext _db;
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
