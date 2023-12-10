a0dceadbfa5af2e66493ee750ac7283516899006
qqhieubb <122640990+qqhieubb@users.noreply.github.com>
2023-12-10 23:23:51 +0700

    Tạo Edit và Delete cho Category

--- LienXoChongUS/Controllers/CategoryController.cs
+++ LienXoChongUS/Controllers/CategoryController.cs
@@ -7,9 +7,12 @@ namespace LienXoChongUS.Controllers
     public class CategoryController : Controller
     {
         private readonly AppDbContext _db;
-        public CategoryController(AppDbContext db) {
-            _db = db;        
+
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
         }
+
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
@@ -24,13 +27,85 @@ namespace LienXoChongUS.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Name do not same with the DisplayOrder");
+            }
+            if (obj.Name != null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is an invalid Value");
+            }
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["SuccessMessage"] = "Category created successfully!";
                 return RedirectToAction("Index");
             }
             return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Name do not same with the DisplayOrder");
+            }
+            if (obj.Name != null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is invalid Value");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["SuccessMessage"] = "Category updated successfully!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["SuccessMessage"] = "Category deleted successfully!";
+            return RedirectToAction("Index");
         }
     }
 }
