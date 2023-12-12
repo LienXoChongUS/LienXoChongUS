8b00e6e8743f0b728cb04c78b6a819194da0503b
BrownKumano <112859155+BrownKumano@users.noreply.github.com>
2023-12-12 17:30:51 +0700

    change register

--- LienXoChongUS/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ LienXoChongUS/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -2,14 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Text.Encodings.Web;
-using System.Threading;
-using System.Threading.Tasks;
+using LienXoChongUS.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
