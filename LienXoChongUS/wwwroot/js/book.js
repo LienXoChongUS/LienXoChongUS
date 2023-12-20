791073927208aa693be0be32921eacaf1a41296f
qqhieubb <122640990+qqhieubb@users.noreply.github.com>
2023-12-20 17:18:54 +0700

    Trả nút Edit và Delete, nhưng mới sửa lại Edit

--- LienXoChongUS/wwwroot/js/book.js
+++ LienXoChongUS/wwwroot/js/book.js
@@ -8,10 +8,21 @@ function loadDataTable() {
             { data: 'id',"width":"5%" },
             { data: 'title', "width": "10%" },
             { data: 'description', "width": "30%" },
-            { data: 'author', "width": "10%" },
+            { data: 'author', "width": "5%" },
             { data: 'price', "width": "5%" },
-            { data: 'category.name', "width": "10%" }
-            
+            { data: 'category.name', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                    <a href="/Admin/Book/UpSert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i> Edit</a>
+                    <a asp-controller="Book" asp-action="Delete" asp-route-id="@obj.Id" class="btn btn-delete mx-2"><i class="bi bi-trash3"></i> Delete</a>
+                    </div>`
+                        
+                    
+                },
+                "width": "30%"
+            }
         ]
     });
 }
