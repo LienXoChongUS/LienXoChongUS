71d2acc7a6f10219c9f33c10beee16a09e9e29a4
Nguyen Thanh Phong <96514239+TrH-In-GsH@users.noreply.github.com>
2023-12-26 01:58:22 +0700

    Update ApplicationUserRepository.cs

--- LXxUS.DataAccess/Repository/ApplicationUserRepository.cs
+++ LXxUS.DataAccess/Repository/ApplicationUserRepository.cs
@@ -10,8 +10,16 @@ using System.Threading.Tasks;
 
 namespace LXxUS.DataAccess.Repository
 {
-    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+
+/*ApplicationUserRepository là một class kế thừa từ Repository<ApplicationUser> và thực hiện interface IApplicationUserRepository. 
+  Thừa hưởng các phương thức chung từ Repository và triển khai các phương thức từ IApplicationUserRepository.
+*/
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository IApplicationUserRepository.
     {
+
+        /*onstructor của ApplicationUserRepository nhận một tham số kiểu AppDbContext
+          Nó gọi constructor của lớp cơ sở Repository<ApplicationUser> và gán giá trị db vào biến _db.
+        */ 
         private AppDbContext _db;
         public ApplicationUserRepository(AppDbContext db) : base(db) { 
             _db = db;   
