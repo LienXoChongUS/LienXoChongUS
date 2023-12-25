c1f1022ec0936f676fb38bda37e87ddfaa8cadd7
Nguyen Thanh Phong <96514239+TrH-In-GsH@users.noreply.github.com>
2023-12-26 01:54:11 +0700

    Update 20231221192550_Update2.cs

--- LXxUS.DataAccess/Migrations/20231221192550_Update2.cs
+++ LXxUS.DataAccess/Migrations/20231221192550_Update2.cs
@@ -20,3 +20,25 @@ namespace LXxUS.DataAccess.Migrations
         }
     }
 }
+/*
+Dưới đây là giải thích về một số phần quan trọng trong file migration này:
+
+using Microsoft.EntityFrameworkCore.Migrations;:
+
+Dòng này thêm một namespace cho các lớp migration của Entity Framework Core.
+#nullable disable:
+
+Dòng này tắt tính năng nullable cho toàn bộ file. Tức là, trong phạm vi của file này, tất cả các kiểu dữ liệu đều được giả định là không nullable.
+namespace LXxUS.DataAccess.Migrations:
+
+Định nghĩa không gian tên của file migration.
+public partial class Update2 : Migration:
+
+Đây là một lớp có tên Update2 mà bạn tạo ra cho migration. Mỗi migration được đại diện bởi một lớp và phải kế thừa từ lớp Migration của Entity Framework Core.
+protected override void Up(MigrationBuilder migrationBuilder):
+
+Phương thức Up được gọi khi bạn muốn áp dụng các thay đổi cơ sở dữ liệu. Trong phương thức này, bạn sẽ định nghĩa các thao tác SQL để thực hiện thay đổi (thêm bảng, chỉnh sửa cột, v.v.).
+protected override void Down(MigrationBuilder migrationBuilder):
+
+Phương thức Down được gọi khi bạn muốn hủy bỏ các thay đổi cơ sở dữ liệu. Trong phương thức này, bạn sẽ định nghĩa các thao tác SQL để đảo ngược các thay đổi đã áp dụng trong phương thức Up.
+*/
