3ca1666036d7cc2c73b5a97471e5b2a951be6f52
Tyends41 <141699209+Tyends41@users.noreply.github.com>
2023-12-28 17:36:45 +0700

    Add past order view for customers

--- LXxUS.DataAccess/Repository/ApplicationUserRepository.cs
+++ LXxUS.DataAccess/Repository/ApplicationUserRepository.cs
@@ -14,7 +14,7 @@ namespace LXxUS.DataAccess.Repository
 /*ApplicationUserRepository là một class kế thừa từ Repository<ApplicationUser> và thực hiện interface IApplicationUserRepository. 
   Thừa hưởng các phương thức chung từ Repository và triển khai các phương thức từ IApplicationUserRepository.
 */
-    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository IApplicationUserRepository.
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository 
     {
 
         /*onstructor của ApplicationUserRepository nhận một tham số kiểu AppDbContext
